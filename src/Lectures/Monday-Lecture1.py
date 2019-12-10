47a15b8d2c9e473dccd4cd9092532d9e4488bdc7
lschimm <lschimmelfennig@gmail.com>
2019-12-09 21:13:15 -0600

    Adds more notes to Monday-Lecture

--- src/Lectures/Monday-Lecture1.py
+++ src/Lectures/Monday-Lecture1.py
@@ -5,13 +5,24 @@
 # 2) n! = (n) * (n-1) * (n-2) ....
 # Base Case: if n <= 1, return 1
 
-def factorial(n):
+# After try 1: Realized recursion cannot work with nunbers >= 999
+# Try 2: Try iterative
+
+def recursive_factorial(n): # O(n) linear!
     if n <= 1:
         return 1
-    return n * factorial(n - 1)
+    return n * recursive_factorial(n - 1)
 
 
-print(factorial(11)) # 39916800
-print(factorial(10)) # 3628800
-print(factorial(2))  # 2
-print(factorial(0)) # 1
+# print(recursive_factorial(11)) # 39916800
+# print(recursive_factorial(10)) # 3628800
+# print(recursive_factorial(2))  # 2
+# print(recursive_factorial(0)) # 1
+
+def iterative_factorial(n):
+    result = 1 
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+print(iterative_factorial(1000)) # biiiiiig number
